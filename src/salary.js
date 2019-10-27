8d7bff36cc97504ba1770b98dab4d0cbe4b1bdc0
Kaela Maselli <ksmaselli@gmail.com>
2019-10-27 17:58:07 -0400

    Update salary.js

--- src/salary.js
+++ src/salary.js
@@ -1,1 +1,18 @@
+const readlineSync = require("readline-sync");
+const annual = Number(readlineSync.question("\nAnnual salary: "));
 
+const pretax = 0.93;
+
+const federal = 0.157;
+const state = 0.0447;
+const social = 0.062;
+const medicare = 0.0145;
+
+const salary = (pretax*annual);
+const total = ((salary*federal) + (salary*state) + (salary*social) + (salary*medicare));
+
+const home = (salary-total);
+
+let paycheck = (home / 24).toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2});
+
+console.log("\nYour take-home pay each check will be $" + paycheck + ".")
